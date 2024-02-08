c54b918f4777d19a8f0428af71763631be533cb4
PaigeBrown-27 <paige.brown@students.fhu.edu>
2024-02-08 15:42:09 -0600

    first_paige
    First time uploading!

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    pass
+    if len(list1) == len(list2):
+        list_products = []
+        for i in range (0, len(list1)):
+            list_products.append(list1[i] * list2[i])
+        end_num = sum(list_products)
+        return end_num
+    else:
+       return "Error"
 
 if __name__ == '__main__':
-   #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    num_list1 = []
+    num_list2 = []
+    nl1 = input()
+    nl2 = input()
+    
+    for i in range (0, len(nl1)):
+        num_list1.append(int(nl1[i]))
+    for i in range (0, len(nl2)):
+        num_list2.append(int(nl2[i]))
+    #num_list1 = [int(input("Give me a list of numbers: "))]
+    #num_list2 = [int(input("Give me a second list of numbers: "))]
+    print(sum_of_products(num_list1, num_list2))
