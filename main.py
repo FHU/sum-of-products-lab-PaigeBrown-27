d3ee7db1e53d54cb002a2a41b2110b49a889aa02
PaigeBrown-27 <paige.brown@students.fhu.edu>
2024-03-06 12:24:22 -0600

    I know this is late

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 def sum_of_products(list1, list2):
     if len(list1) == len(list2):
         list_products = []
-        for i in range (0, len(list1)):
+        for i in range (0, len(list1) -1):
             list_products.append(list1[i] * list2[i])
         end_num = sum(list_products)
-        return end_num
+        print(end_num)
     else:
-       return "Error"
+       print("Error")
 
 if __name__ == '__main__':
     num_list1 = []
@@ -22,4 +22,4 @@ if __name__ == '__main__':
         num_list2.append(int(nl2[i]))
     #num_list1 = [int(input("Give me a list of numbers: "))]
     #num_list2 = [int(input("Give me a second list of numbers: "))]
-    print(sum_of_products(num_list1, num_list2))
+    sum_of_products(num_list1, num_list2)
